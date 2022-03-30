7e1cf7ed6e2739605b54ba2a2810a8d587eaa9dd
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-03-30 08:29:03 +0530

    Added::Welcome to Address book Program

---
+++ src/com/bridgrlabz/javaproject/Addressbook_Main.java
@@ -0,0 +1,10 @@
+package com.bridgrlabz.javaproject;
+
+public class Addressbook_Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+
+	}
+
+}
